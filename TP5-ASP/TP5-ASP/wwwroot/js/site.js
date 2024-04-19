1d07440c82b83016fb40c1ec678b620347d26e8b
Nelson Junior Yimou Noubissi <2130331@etu.cchic.ca>
2024-04-19 13:43:35 -0400

    Terminé complètement avec les astuce du prof vendredi

--- TP5-ASP/TP5-ASP/wwwroot/js/site.js
+++ TP5-ASP/TP5-ASP/wwwroot/js/site.js
@@ -9,10 +9,9 @@ $(document).ready(function () {
             at: 'center bottom'
         }
     });
-});
-
-$(function () {
-    $("#datepicker").datepicker();
+    $("#datepicker").datetpicker({
+        dateFormat: 'YY-MM-DD'
+    });
 });
 
 
